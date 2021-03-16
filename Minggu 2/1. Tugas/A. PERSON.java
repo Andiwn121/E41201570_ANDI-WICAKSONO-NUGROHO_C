c2f69f88a71edfdde4d7b621a6fa1c8ea01adc00
Andi Wicaksono Nugroho <75154388+Andiwn121@users.noreply.github.com>
2021-03-16 11:57:18 +0700

    Create

---
+++ Minggu 2/1. Tugas/A. PERSON.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas;
+
+/**
+ *
+ * @author asus
+ */
+public class PERSON {
+    public static void main(String[] args){
+        
+        String fName = "Lisa" , lName = "Palombo" , stuStatus = "Active";
+        int stuId = 123456789;
+        
+        System.out.println("Student Name : " + fName + " " + lName);
+        System.out.println("Student Id : " + stuId);
+        System.out.println("Student Status : " + stuStatus);
+    }
+}
---
+++ Minggu 2/1. Tugas/C. Cek.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas;
+
+/**
+ *
+ * @author asus
+ */
+public class Cek {
+    boolean gameOver = false;
+    int student = 50 , classes = 30;
+    double sales_tax;
+    short number1;
+    int 2beOrNot2be; //kesalahan karena menuliskan angka diawal variabel
+    
+    float price index; //kesalahan karena adanya spasi diantara kata price dan index
+    
+    double lastYear'sPrice; 
+    //kesalahan karena menggunakan symbol (')
+    
+    long class; 
+    // kesalahan karena menggunakan keyword asli dari java
+                
+}
