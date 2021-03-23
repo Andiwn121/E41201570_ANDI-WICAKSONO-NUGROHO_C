72322c3014dea0a7f1f9d0b59b02527304152af9
Andi Wicaksono Nugroho <75154388+Andiwn121@users.noreply.github.com>
2021-03-23 14:06:29 +0700

    Update a. Array.java

--- Minggu 3/Tugas/a. Array.java
+++ Minggu 3/Tugas/a. Array.java
@@ -3,28 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-import java.util.Scanner;
 /**
  *
  * @author asus
  */
 public class Array {
     public static void main(String[] args) {
-        String fName, mName, lName;
+        String [] nama = {"Andi", "Wicaksono", "Nugroho"};
     
-        Scanner data = new Scanner(System.in);
-        System.out.println("Input Data Diri");        
-        System.out.println("");
-        System.out.print("Nama Depan     : ");
-        fName = data.nextLine();
-        System.out.print("Nama Tengah    : ");
-        mName = data.nextLine();
-        System.out.print("Nama Belakang  : ");
-        lName = data.nextLine();
-        
-        //hasil inputan
-        System.out.println("");
-        System.out.println("**********");
-        System.out.println("Nama     : " +fName+ " " +mName+ " " +lName);
+        for (int i=0; i<nama.length; i++){
+            System.out.print(nama[i]+"");
+            System.out.print(" ");
+            System.out.print(" ");
+        }
     }    
 }
