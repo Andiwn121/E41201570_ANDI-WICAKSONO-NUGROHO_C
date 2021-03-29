aa914292f5d2ce4783f615aa98173645765bbc04
Andi Wicaksono Nugroho <75154388+Andiwn121@users.noreply.github.com>
2021-03-29 14:19:58 +0700

    Create

---
+++ MInggu 4/Tugas/b. Kelipatan.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w4;
+
+
+
+/**
+ *
+ * @author asus
+ */
+public class Kelipatan {
+ public static void main(String[] args) {
+      
+        
+        int i = 1;
+        
+        System.out.println("        Do_While        ");
+        System.out.println("   Kelipatan 2 (1-100)  ");
+        System.out.println("========================");
+        do {
+            System.out.print(" "+i+" ");
+
+            i*=2;
+            
+        } while (i<=100);
+        
+     System.out.println("");
+    }           
+}
